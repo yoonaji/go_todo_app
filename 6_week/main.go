80cacb2f6aadee477c6f841591200d5bb1cbc252
yoonaji <168801970+yoonaji@users.noreply.github.com>
2024-11-12 21:28:44 +0900

    Add files via upload

---
+++ 6_week/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go_todo_app/config" // 이 부분은 실제 config 패키지 경로로 수정하세요.
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
+}
+
+
